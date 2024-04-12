08d9498a71735f5146a2046c0279bb14e627fbd0
Matt McCormick <matt.mccormick@kitware.com>
2024-04-12 16:05:17 -0400

    Only use clang pragma with clang
    /home/matt/src/libcbor/src/cbor.c:12: warning: ignoring ‘#pragma clang diagnostic’ [-Wunknown-pragmas]
       12 | #pragma clang diagnostic push
          |

--- src/cbor.c
+++ src/cbor.c
@@ -9,7 +9,9 @@
 #include "cbor/internal/builder_callbacks.h"
 #include "cbor/internal/loaders.h"
 
+#if defined(__clang__)
 #pragma clang diagnostic push
+#endif
 cbor_item_t *cbor_load(cbor_data source, size_t source_size,
                        struct cbor_load_result *result) {
   /* Context stack */
